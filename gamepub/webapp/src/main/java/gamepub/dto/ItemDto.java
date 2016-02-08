9bd81c200c098d3798fff8273cfe317961f986eb
anatoliy-sh <an_sheb@mail.ru>
2016-02-08 15:05:01 +0300

    Add search on main page, fix User Dao

---
+++ gamepub/webapp/src/main/java/gamepub/dto/ItemDto.java
@@ -0,0 +1,16 @@
+package gamepub.dto;
+
+/**
+ * Created by Анатолий on 08.02.2016.
+ */
+public class ItemDto {
+    String avatarUrl;
+    String link;
+
+    public String getAvatarUrl(){
+        return avatarUrl;
+    }
+    public String getLink(){
+        return link;
+    }
+}
